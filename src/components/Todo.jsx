834bd15733b137e0bbc55f586b4269dadfdf60e4
johntran <tranjohn1211@gmail.com>
2022-07-25 23:52:22 -0700

    add complete button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,13 +5,37 @@ const Todo = () => {
     const [todoList, setTodoList] = useState([])
     const [newTask, setNewTask] = useState("")
 
+    console.log(todoList)
+
     const handleChange =(e) => {
         setNewTask(e.target.value)
     }
 
     const addTask = () => {
-        const newTodoList = [...todoList, newTask]
+        const task ={
+            id: todoList.length === 0 ? 1 : todoList[todoList.length-1].id +1,
+            taskname: newTask,
+            complete: false
+        }
+        const newTodoList = [...todoList, task]
         setTodoList(newTodoList)
+    }
+
+    const deleteTask = (id) => {
+        const newTodoList = todoList.filter((task)=> {
+            return task.id !==id
+        })
+        setTodoList(newTodoList)
+    }
+
+    const completeTask = (id) => {
+        setTodoList(todoList.map((task) => {
+            if(task.id === id) {
+                return {...task, complete: true} ;
+            } else {
+                return task;
+            }
+        }))
     }
     return ( 
         <div>
@@ -21,11 +45,18 @@ const Todo = () => {
                 <button onClick={addTask}>Add Task</button>
             </div>
             <div className="list">
-                {todoList.map((todo) => {
-                    return <h1>{todo}</h1>
+                {todoList.map((task, id) => {
+                    return (
+                        <div key ={id} className='task' style={{backgroundColor: task.complete  ? 'green' :'white'}} >
+                            <h1>{task.taskname} {task.id}</h1>
+                            <button onClick={()=>deleteTask(task.id)}>X</button>
+                            <button onClick={()=>completeTask(task.id)}>COMPLETE</button>
+                        </div>
+                    )
                         
                     
                 })}
+               
             </div>
             </div> 
         </div>
