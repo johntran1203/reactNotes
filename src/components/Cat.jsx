a8c5f2654b42f86aac46b0b36303848650921add
johntran <tranjohn1211@gmail.com>
2022-07-28 01:56:23 -0700

    age api

--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -9,6 +9,8 @@ const Cat = () => {
     //     console.log(data)
     // })
     const [catFact, setCatFat] = useState("")
+    const [name, setName] = useState('')
+    const [age, setAge] = useState(null)
 
     const fetchCatFact = () => {
         Axios.get('https://catfact.ninja/fact').then((res) => {
@@ -19,11 +21,23 @@ const Cat = () => {
        fetchCatFact()
 
     }, [])
+    const fetchData = () => {
+        Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+            setAge(res.data)
+        })
+    }
+
     return ( <div>
         <h2>CAT</h2>
         <button onClick={fetchCatFact}>Generate Cat Fact</button>
         <p>{catFact}</p>
+        <hr />
+        <input placeholder="EX. tony"  onChange={(e)=>{setName(e.target.value)}}/>
+        <button onClick={fetchData}>Predict AGE</button>
+        <h2>Predicted name: {age?.name}</h2>
+        <h2>Predicted AGE: {age?.age}</h2>
+        <h2>Predicted count: {age?.count}</h2>
     </div> );
 }
- 
+
 export default Cat;
